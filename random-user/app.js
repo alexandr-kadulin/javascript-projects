86c59611f55ea5de04c4f803a7b06830f8557853
Aleksandr Kadulin <alexandr-kadulin@hotmail.com>
2022-03-29 14:23:10 +0300

    initial commit

---
+++ random-user/app.js
@@ -0,0 +1,13 @@
+import getUser from "./utils/getUser.js";
+import getElement from "./utils/getElement.js";
+import displayUser from "./utils/displayUser.js";
+
+const btn = getElement(".btn");
+
+const showUser = async () => {
+  const person = await getUser();
+  displayUser(person);
+};
+
+window.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);
