b3bf0174a1824ff1ccad6a92fd76fbfaf299b1d1
Aleksandr Kadulin <alexandr-kadulin@hotmail.com>
2022-03-29 14:23:10 +0300

    add random-user

---
+++ random-user/utils/displayUser.js
@@ -0,0 +1,30 @@
+import getElement from "./getElement.js";
+
+const img = getElement(".user-img");
+const title = getElement(".user-title");
+const value = getElement(".user-value");
+const btns = [...document.querySelectorAll(".icon")];
+
+const displayUser = (person) => {
+  img.src = person.image;
+  value.textContent = person.name;
+  title.textContent = `My name is`;
+  removeActive(btns);
+  btns[0].classList.add("active");
+
+  btns.forEach((btn) => {
+    const label = btn.dataset.label;
+    btn.addEventListener("click", () => {
+      title.textContent = `My ${label} is`;
+      value.textContent = person[label];
+      removeActive(btns);
+      btn.classList.add("active");
+    });
+  });
+};
+
+function removeActive(items) {
+  items.forEach((item) => item.classList.remove("active"));
+}
+
+export default displayUser;
---
+++ random-user/utils/getElement.js
@@ -0,0 +1,11 @@
+function getElement(selection) {
+  const element = document.querySelector(selection);
+
+  if (element) {
+    return element;
+  }
+
+  throw new Error("no element selected");
+}
+
+export default getElement;
