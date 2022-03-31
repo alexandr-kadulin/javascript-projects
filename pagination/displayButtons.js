4b43c8fb9e9f7ff3f5f04ff50fbd5eb98a7be857
Aleksandr Kadulin <alexandr-kadulin@hotmail.com>
2022-03-31 16:33:04 +0300

    add pagination

---
+++ pagination/displayButtons.js
@@ -0,0 +1,18 @@
+const displayButtons = (btnContainer, pages, index) => {
+  let btns = pages.map((_, pageIndex) => {
+    return `
+			<button class="page-btn ${
+        index === pageIndex ? "active-btn" : null
+      }" data-index="${pageIndex}">
+				${pageIndex + 1}
+			</button>
+		`;
+  });
+
+  btns.push(`<button class="next-btn">next</button>`);
+  btns.unshift(`<button class="prev-btn">prev</button>`);
+
+  btnContainer.innerHTML = btns.join("");
+};
+
+export default displayButtons;
